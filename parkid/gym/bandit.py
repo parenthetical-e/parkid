e42985a4f6c4dd96eb7d4d9629bc897f59fd150e
Erik Peterson <Erik.Exists@gmail.com>
2020-12-01 14:41:16 -0800

    Change bandits to be more like emily's

--- parkid/gym/bandit.py
+++ parkid/gym/bandit.py
@@ -70,9 +70,73 @@ class BanditEnv(gym.Env):
         pass
 
 
+# TODO add an emily model
+
+
+class BanditUniform4(BanditEnv):
+    """A 4 armed bandit."""
+    def __init__(self, p_min=0.1, p_max=0.3, p_best=0.6, best=2):
+        self.best = [best]
+        self.num_arms = 4
+
+        # ---
+        self.p_min = p_min
+        self.p_max = p_max
+        self.p_best = p_best
+
+        # Generate intial p_dist
+        # (gets overwritten is seed())
+        p_dist = np.random.uniform(self.p_min, self.p_max,
+                                   size=self.num_arms).tolist()
+        p_dist[self.best[0]] = self.p_best
+
+        # reward
+        r_dist = [1] * self.num_arms
+
+        # ---
+        BanditEnv.__init__(self, p_dist=p_dist, r_dist=r_dist)
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+
+        # Reset p(R) dist with the seed
+        self.p_dist = self.np_random.uniform(self.p_min,
+                                             self.p_max,
+                                             size=self.num_arms).tolist()
+        self.p_dist[self.best[0]] = self.p_best
+
+        return [seed]
+
+
+class BanditChange4(BanditUniform4):
+    """Change the worst choice to the best BanditUniform4"""
+    def __init__(self,
+                 p_min=0.1,
+                 p_max=0.3,
+                 p_best=0.99,
+                 p_org=0.6,
+                 org_best=2):
+        super().__init__(p_min=p_min, p_max=p_max, p_best=p_org, best=org_best)
+
+        # Save the old best
+        self.original_best = deepcopy(self.best)
+        self.p_best = p_best
+
+        # Make the worst the best
+        self.best = [np.argmin(self.p_dist)]
+        self.p_dist[self.best[0]] = self.p_best
+
+    def seed(self, seed=None):
+        super().seed(seed)
+        self.best = [np.argmin(self.p_dist)]
+        self.p_dist[self.best[0]] = self.p_best
+
+        return [seed]
+
+
 class BanditUniform121(BanditEnv):
     """A 121 armed bandit."""
-    def __init__(self, p_min=0.2, p_max=0.8, p_best=0.8, best=54):
+    def __init__(self, p_min=0.1, p_max=0.3, p_best=0.6, best=54):
         self.best = [best]
         self.num_arms = 121
 
@@ -106,24 +170,26 @@ class BanditUniform121(BanditEnv):
 
 
 class BanditChange121(BanditUniform121):
-    """Change the best choice in BanditUniform121 to the worst"""
-    def __init__(self, p_min=0.2, p_max=0.8, p_best=0.8, org_best=54):
-        super().__init__(p_min=p_min,
-                         p_max=p_max,
-                         p_best=p_best,
-                         best=org_best)
+    """Change the worst choice to the best BanditUniform121"""
+    def __init__(self,
+                 p_min=0.1,
+                 p_max=0.3,
+                 p_best=0.99,
+                 p_org=0.6,
+                 org_best=54):
+        super().__init__(p_min=p_min, p_max=p_max, p_best=p_org, best=org_best)
 
-        # Make the best the worst
+        # Save the old best
         self.original_best = deepcopy(self.best)
-        self.p_dist[self.original_best[0]] = self.p_min
+        self.p_best = p_best
 
-        # Now update the best
-        self.best = [np.argmax(self.p_dist)]
+        # Make the worst the best
+        self.best = [np.argmin(self.p_dist)]
+        self.p_dist[self.best[0]] = self.p_best
 
     def seed(self, seed=None):
         super().seed(seed)
-        self.original_best = deepcopy(self.best)
-        self.p_dist[self.original_best[0]] = self.p_min
-        self.best = [np.argmax(self.p_dist)]
+        self.best = [np.argmin(self.p_dist)]
+        self.p_dist[self.best[0]] = self.p_best
 
         return [seed]
