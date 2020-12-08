5809f48a73addaea4328186137d593ac3cb8e957
Erik Peterson <Erik.Exists@gmail.com>
2020-12-08 15:05:12 -0800

    Tweak/fix homeostasis in kids

--- parkid/utils.py
+++ parkid/utils.py
@@ -7,7 +7,5 @@ def R_homeostasis(reward, total_reward, set_point):
     Value based on Keramati and Gutkin, 2014.
     https://elifesciences.org/articles/04811
     """
-    deviance_last = np.abs(set_point - total_reward)
-    deviance = np.abs(set_point - (total_reward + reward))
-    reward_value = deviance_last - deviance
-    return reward_value
+    deviance = (set_point - total_reward) / set_point
+    return deviance + (reward / set_point)
