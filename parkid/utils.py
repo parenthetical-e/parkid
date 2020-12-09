50b6d889da60f4d2bb3b0a53d6a1c18333284d90
Erik Peterson <Erik.Exists@gmail.com>
2020-12-09 11:32:27 -0800

    Fix edge cases in H def

--- parkid/utils.py
+++ parkid/utils.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 
-def R_homeostasis(reward, total_reward, set_point):
+def R_homeostasis(reward, total_reward, set_point, cost=0.0):
     """Update reward value assuming homeostatic value.
     
     Value based on Keramati and Gutkin, 2014.
     https://elifesciences.org/articles/04811
     """
-    deviance = (set_point - total_reward) / set_point
-    return deviance + (reward / set_point)
+    if np.isclose(set_point, np.inf):
+        return reward
+    if np.isclose(set_point, 0.0):
+        return reward
+    if np.isclose(reward, 0.0):
+        return 0.0
+
+    deviance = set_point - total_reward
+    h = (deviance + reward - cost) / set_point
+
+    return np.clip(h, a_min=-+0.0, a_max=np.inf)
